3d00f3d0e4dab1f7a3609fe4b8bdb40c38ccbdc1
Laurent DIAZ <laurentdiazfr@gmail.com>
2025-10-17 13:01:14 +0200

    fix commit

---
+++ SimpleApp/App.tsx
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HybridTest1, HybridTest2View } from 'simple-lib';
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  return (
+    <View style={styles.container}>
+      <Text>HybridTest1</Text>
+      <Text>1 + 2 = {HybridTest1.add(1, 2)} </Text>
+      <Text>HybridTest2</Text>
+      <HybridTest2View enableFlash={true} style={styles.test2} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  test2: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default App;
---
+++ SimpleApp/metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+const root = path.join(__dirname, '..');
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
+    watchFolders: [root],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
